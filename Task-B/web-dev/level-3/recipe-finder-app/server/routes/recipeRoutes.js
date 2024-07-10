ec57374da2f7ece7b3dc750ad537941a198ad20d
adey-github <155592377+adey-github@users.noreply.github.com>
2024-07-11 01:57:12 +0530

    Added Web dev task

--- Task-B/web-dev/level-3/recipe-finder-app/server/routes/recipeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example route definition
-router.get('/recipes', (req, res) => {
-    res.send('This is the recipes endpoint');
-});
-
-module.exports = router;
